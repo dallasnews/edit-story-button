68f7132c9a8415254a1a1db43451890379a35207
claire <claire.campbell@dallasnews.com>
2021-05-19 17:56:05 -0700

    update build process for terser v5 promise-based API

--- build.js
+++ build.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const { minify } = require('terser')
 
-// Minify code
-const res = minify(fs.readFileSync('index.js', { encoding: 'utf8' }))
-if (res.error) throw new Error(res.error)
-const code = encodeURI(res.code)
-
-// Spit out bookmarklet code
-console.log(`javascript:${code}`)
+// Minify source and output bookmarklet code
+const source = fs.readFileSync('index.js', { encoding: 'utf8' })
+minify(source).then(({ code }) => console.log(`javascript:${encodeURI(code)}`))
