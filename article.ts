bc8e07e1264f349bcd756b0f6b7ba5f2c5c2e222
claire <claire.campbell@dallasnews.com>
2021-05-20 12:20:38 -0700

    add AMP article ID logic

--- article.ts
+++ article.ts
@@ -7,11 +7,40 @@ interface FusionGlobal {
   globalContent: GlobalContent
 }
 
+interface AnalyticsConfigVars {
+  articleId: string
+}
+
+interface AnalyticsConfig {
+  vars: AnalyticsConfigVars
+}
+
 declare const Fusion: FusionGlobal
+
+const findAMPAnaylticsArticleId = (): string | null => {
+  const configSelector = 'amp-analytics > script[type$=json]'
+  const configElements = [...document.querySelectorAll(configSelector)]
+  const articleId = configElements
+    .map((scriptEl) => {
+      let data: AnalyticsConfig | null
+
+      try {
+        data = JSON.parse(scriptEl.innerHTML)
+      } catch (e) {
+        data = null
+      }
+
+      return data?.vars?.articleId
+    })
+    .find((c) => c /* return the first non-empty item */)
+
+  return articleId || null
+}
 
 const findFusionArticleId = (): string | null => {
   const content = Fusion?.globalContent
   return (content?.type === 'story' && content?._id) || null
 }
 
-export const getArticleId = (): string | null => findFusionArticleId()
+export const getArticleId = (): string | null =>
+  findFusionArticleId() || findAMPAnaylticsArticleId()
